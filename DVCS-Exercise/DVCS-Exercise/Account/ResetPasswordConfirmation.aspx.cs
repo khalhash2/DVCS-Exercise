60c2a0c1d7edad204c90abc0af6b68f585ca67e9
khalhash2 <35302426+khalhash2@users.noreply.github.com>
2019-09-24 09:12:09 -0600

    created DVCS website

---
+++ DVCS-Exercise/DVCS-Exercise/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DVCS_Exercise.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DVCS-Exercise/DVCS-Exercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DVCS_Exercise.Global" Language="C#" %>
